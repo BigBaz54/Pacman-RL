fe506fb95e530d825702677f80baf143b027c6f1
BigBaz <louisvincent@famillecapelli.fr>
2024-02-22 20:08:12 -0500

    [+] adding q learning run to main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from value_iteration import ValueIteration
+from q_learning import QLearning
 
 
 # Create an instance of the ValueIteration class that reads the environment from the file 'value-iteration.txt'
@@ -8,3 +9,11 @@ vi = ValueIteration('value-iteration.txt')
 vi.value_iteration(trace=True)
 # Compute the final policy and store the policy for each state in self.policy.
 vi.compute_policy(trace=True)
+
+
+#Create an instance of the QLearning class that reads the environment from the file 'Q-Learning.txt'
+# and create its own GameEnv object.
+ql = QLearning('Q-Learning.txt')
+# Run the Q-learning algorithm and store the Q-values for each state-action pair in self.q_values
+# and the policy for each state in self.policy.
+ql.train(trace=True)
